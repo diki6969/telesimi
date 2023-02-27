bed56032ceaf02d64858c64aa50af70a747dd9fb
diki6969 <gendonmenjeng@gmail.com>
2023-02-27 21:06:26 +0700

    Update simi.py

--- simi.py
+++ simi.py
@@ -1,18 +1,25 @@
-import telegram
-from telegram.ext import Updater, CommandHandler
+// Membuat bot Telegram
+const TelegramBot = require('node-telegram-bot-api');
+const token = '6059060665:AAG0LZLVdkPR6CHVKWtdagvTBHd-krkMRrI';
+const bot = new TelegramBot(token, {polling: true});
 
+// Menangani pesan yang dikirim ke bot
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text;
 
-def handle_message(bot, update):
-    text = update.message.text
-    # Kirim permintaan ke API Simi Simi
-    response = requests.get('https://api.simsimi.net/v2/?text=' + text + '&lc=id')
-    # Ambil respons dari API
-    response_text = response.json()['success']
-    # Kirim respons ke pengguna
-    bot.send_message(chat_id=update.message.chat_id, text=response_text)
-    
-updater = Updater(token='6059060665:AAG0LZLVdkPR6CHVKWtdagvTBHd-krkMRrI')
-dispatcher = updater.dispatcher
-dispatcher.add_handler(CommandHandler('message', handle_message))
+  // Mengirim permintaan ke API Simi Simi
+  const request = require('request');
+  const url = `https://api.simsimi.net/v2/?text=${text}&lc=id`;
+  request(url, (err, response, body) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const data = JSON.parse(body);
+      const responseText = data.response;
 
-updater.start_polling()
+      // Mengirim respons ke chat
+      bot.sendMessage(chatId, responseText);
+    }
+  });
+});
